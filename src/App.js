8c86c050d4d9036883f62d74c80c5654ab287fb0
FediRouatbi <fedirouatbi@gmail.com>
2022-07-15 09:38:50 +0100

    mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import Profile from "./Pages/Profile";
+import Expenses from "./Pages/Expenses";
+import Settings from "./Pages/Settings";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PhoneNav from "./components/PhoneNav";
+import SingIn from "./Pages/SingIn";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="  py-16   h-screen">
+        <Header />
+        <div className="mx-auto  w-11/12  overflow-auto h-full pt-3">
+          <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/expenses" element={<Expenses />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/signin" element={<SingIn />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </Routes>
+        </div>
+        <PhoneNav />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Stats from "../components/Stats";
+import ShowAll from "../components/ShowAll";
+import Table from "../components/Table";
+import Chart from "../components/Chart";
+const Profile = () => {
+  return (
+    <>
+      <Stats />
+      <ShowAll />
+      <Table />
+      <Chart />
+  
+    </>
+  );
+};
+
+export default Profile;
