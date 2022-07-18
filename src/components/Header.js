74bf3478b85887f51651d88becaf70fc903ec853
FediRouatbi <fedirouatbi@gmail.com>
2022-07-18 12:31:26 +0100

    add user img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { GetData } from "../appContext/AppContext";
+import userImg from "./img/user.jpg";
 const Header = () => {
   const { currentUser, signOutF } = GetData();
 
@@ -22,7 +23,7 @@ const Header = () => {
           <div className="dropdown dropdown-end">
             <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
+                <img src={currentUser?.photoURL || userImg} alt="" />
               </div>
             </label>
             <ul
